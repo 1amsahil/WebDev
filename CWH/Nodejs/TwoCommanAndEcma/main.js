7a8e6fd4ee059485ff1e0f95ee01e2e7917f7af2
1amsahil <sahil981156@gmail.com>
2025-02-25 16:51:36 +0530

    Comman And Ecma Script import

---
+++ CWH/Nodejs/TwoCommanAndEcma/main.js
@@ -0,0 +1,19 @@
+// Comman Javascript :- we use require keyword to access the other package/file function, variable, object etc 
+        //// eg:-  
+
+        const num = require("./commanscript.js");
+        console.log(num);
+
+// EcmaScript :- we use import keyword to access the other package/file function, variable, object etc
+        ////     but add "type" : "module" in package.json  
+        //// eg:- 
+
+// // Name Export
+// import {value} from "./ecmascript.js";
+// console.log(value);
+
+// // Default Export
+// import obj from "./ecmascript.js";
+// console.log(obj);
+
+
